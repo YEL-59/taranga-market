d6749129a45d59c1c4ad5a66f2759c013bc2f882
tofayel <tofayeltuhin143@gmail.com>
2026-01-18 13:17:21 +0600

    all for recent listing card

--- app/(MainLayout)/page.tsx
+++ app/(MainLayout)/page.tsx
@@ -1,11 +1,15 @@
 import Banner from "@/pages/home/banner/Banner";
 import Category from "@/pages/home/category/page";
+import Featured from "@/pages/home/featured/Featured";
+import Recentlist from "@/pages/home/recent/Recentlist";
 
 export default function Home() {
   return (
     <div>
       <Banner />
       <Category />
+      <Featured />
+      <Recentlist />
     </div>
   );
 }
